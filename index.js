b0740f7b881aebf3ced60020435f456f7c54dde1
Abhi123pal <abhinavpal889@gmail.com>
2023-04-12 09:37:49 +0530

    modified Error1

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const quote = document.querySelector("p");
 const person = document.querySelector('h2');
 
 function changeFun(){
-    fetch('https://api.breakingbadquotes.xyz/v1/quotes')
+    fetch('https://programming-quotesapi.vercel.app/api/random')
     .then(res => res.json())
     .then(res => {
-        quote.innerHTML = res[0].quote;
-        person.innerHTML = res[0].author;
+        quote.innerHTML = res.quote;
+        person.innerHTML = res.author;
     }
     );
 }
